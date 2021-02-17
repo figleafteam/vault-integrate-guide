bf91e9f7a0401ac255e7a059aafeba25908ebacb
ddidyk <didyk.d.s@gmail.com>
2021-02-17 18:39:58 +0200

    chore: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {VaultElectronWrapper} from '@figleafteam/vault-electron';
+import {Figleaf} from '@figleafteam/messenger';
+
+const figleaf = new Figleaf();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showVault, setShowVault] = useState(false);
+    const login = () => {
+        figleaf.login('gera+am1sdlkmalskdm@figleaf.com')
+
+        setTimeout(() => setShowVault(true), 5000)
+    }
+    const unlock = () => {
+        (window as any).figleafBackgroundService.unlock()
+
+        setTimeout(() => setShowVault(true), 5000)
+
+    }
+    const getVaultItems = () => {
+        (window as any).figleafBackgroundService.getVaultItems('gera+am1sdlkmalskdm@figleaf.com')
+    }
+    return (
+        <div>
+            <header>
+                <button onClick={login}>Login</button>
+                <button onClick={unlock}>Unlock</button>
+                <button onClick={getVaultItems}>getVaultItems</button>
+                {showVault && <div className="vault">
+                    <VaultElectronWrapper
+                        baseName="vault"
+                        messenger={figleaf.getMessenger()}
+                        openUrl={(url: string) => console.log(url)}
+                    />
+                </div>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
